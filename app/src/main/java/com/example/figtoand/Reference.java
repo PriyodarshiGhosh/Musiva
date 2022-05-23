23a3473d615c2a54afc6b01b74436a306d17fbcc
priyodarshighosh <pghosh2002@yahoo.com>
2022-05-23 22:34:22 +0530

    Transferred from Reference.java to lyrics

--- app/src/main/java/com/example/figtoand/Reference.java
+++ app/src/main/java/com/example/figtoand/Reference.java
@@ -26,43 +26,14 @@ import java.net.URL;
 
 
 public class Reference extends AppCompatActivity {
-    EditText edtArtistName,edtSongName;
-    Button btnGetLyrics;
-    TextView txtLyrics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reference2);
+        setContentView(R.layout.activity_reference);
 //
 
-        edtArtistName = findViewById(R.id.edtArtistName);
-        edtSongName = findViewById(R.id.edtSongName);
-        btnGetLyrics = findViewById(R.id.btnGetLyrics);
-        txtLyrics = findViewById(R.id.txtLyrics);
 
-        btnGetLyrics.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This Button is Tapped", Toast.LENGTH_SHORT).show();
-                String url = "https://api.lyrics.ovh/v1/" + edtArtistName.getText().toString() + "/" +edtSongName.getText().toString();
-                url.replace(" ","20%");
-                RequestQueue requestQueue = Volley.newRequestQueue(Reference.this);
-
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
-                    try{
-                        txtLyrics.setText(response.getString("lyrics"));
-
-                    }catch (JSONException e){
-                        e.printStackTrace();
-                    }
-                },
-                        error -> {
-
-                        });
-
-                requestQueue.add(jsonObjectRequest);
-            }
-        });
     }
 
     }
