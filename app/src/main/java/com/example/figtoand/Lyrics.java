004159bfef9ec8249b77f87a7a38bc4ee307d378
priyodarshighosh <pghosh2002@yahoo.com>
2022-05-23 22:51:27 +0530

    reference modified

--- app/src/main/java/com/example/figtoand/Lyrics.java
+++ app/src/main/java/com/example/figtoand/Lyrics.java
@@ -14,7 +14,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
-
+//
 public class Lyrics extends AppCompatActivity {
     EditText edtArtistName,edtSongName;
     Button btnGetLyrics;
--- app/src/main/java/com/example/figtoand/Reference.java
+++ app/src/main/java/com/example/figtoand/Reference.java
@@ -24,7 +24,7 @@ import org.json.JSONObject;
 
 import java.net.URL;
 
-
+//
 public class Reference extends AppCompatActivity {
 
     @Override
